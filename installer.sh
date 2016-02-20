bd15ebdbc48917b5b135f1330b9fe72de55c08c5
Morjiai <Morjiai@gmail.com>
2016-02-20 04:24:15 -0500

    Changes
    1.) Added -rf for the two directories removed. 
    2.) Removed "done"

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!bin/bash 
 echo "Removing files.."; 
 rm  iui-0.4.1/web-app/iui/iui.js;
-rm  iui-0.4.1/web-app/iui/t;
+rm -rf iui-0.4.1/web-app/iui/t;
 rm  iui-0.4.1/web-app/css/iui-panel-list.css;
 rm  iui-0.4.1/web-app/iui/iui.css;
 echo "Upgrading iUi Framework."; 
@@ -11,10 +11,10 @@ mv iui.css iui-0.4.1/web-app/iui/;
 read -r -p "Would you like to remove tutorials, samples and docs? [y/N] " response
 case $response in
     [yY][eE][sS]|[yY]) 
-        rm iui-0.4.1/web-app/docs iui-0.4.1/web-app/samples iui-0.4.1/web-app/tutorials
+        rm -rf iui-0.4.1/web-app/docs iui-0.4.1/web-app/samples iui-0.4.1/web-app/tutorials
         ;;
     *)
-        echo "Done!"
+        echo "Done!";
         ;;
 esac
-done 
+
