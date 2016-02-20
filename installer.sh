c41e8f5e7e33f06b8cc39c4287702840aa56676a
Morjiai <Morjiai@gmail.com>
2016-02-20 04:02:23 -0500

    How-To
    1.) Extract ModerniUi-iOS in a directory with iUi 0.4.1. 
    2.) Make sure you do not put ModerniUi-iOS in a folder.
    3.) Run the installer.

---
+++ installer.sh
@@ -0,0 +1,20 @@
+#!bin/bash 
+echo "Removing files.."; 
+rm  iui-0.4.1/web-app/iui/iui.js;
+rm  iui-0.4.1/web-app/iui/t;
+rm  iui-0.4.1/web-app/css/iui-panel-list.css;
+rm  iui-0.4.1/web-app/iui/iui.css;
+echo "Upgrading iUi Framework."; 
+mv iui.js iui-0.4.1/web-app/iui/;
+mv pkgs.css iui-0.4.1/web-app/css/;
+mv iui.css iui-0.4.1/web-app/iui/;
+read -r -p "Would you like to remove tutorials, samples and docs? [y/N] " response
+case $response in
+    [yY][eE][sS]|[yY]) 
+        rm iui-0.4.1/web-app/docs iui-0.4.1/web-app/samples iui-0.4.1/web-app/tutorials
+        ;;
+    *)
+        echo "Done!"
+        ;;
+esac
+done 
